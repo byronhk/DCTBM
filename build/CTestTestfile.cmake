1a0069bfd6c0c5b65bdc41f199d145d62d6a8f65
byronhk <byronwanghk@gmail.com>
2023-04-12 18:08:16 +0800

    init commit

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/byron/DCTBM
-# Build directory: /home/byron/DCTBM/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
